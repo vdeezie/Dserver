eff0954343311e7b97622681ce5f77c2322f5d31
Victor Desire <61976018+vdeezie@users.noreply.github.com>
2021-04-03 19:23:12 +0100

    second commit

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const http = require('http');
 
 
 // Create a server with the HTTP variable
-http.createServer(function(req, res){
-    //Headers
+const server = http.createServer(function(req, res){
+    //Header
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    //send back some information
+    res.end(`
+        <html>
+            <body>
+                <h1>Hi, You're welcome to my first HTML Webpage</h1>
+                <p>Let's make progress!</p>
+            </body>
+    
+        </html>
+    `);
 });
+
+//create a port
+server.listen(3000, '127.0.0.1');
+
+console.log("Yes boss, you have created a server!");
